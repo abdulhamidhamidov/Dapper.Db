3f7cf07c6af4dcc8a59535fb6cdb0a13935163a3
Hamitov Abdulhamid <khamitovabdulhamid@gmail.com>
2024-12-06 16:47:05 +0500

    Dapper student

---
+++ MainApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using Infrostructure.Models;
+using Infrostructure.Services;
+
+StudentServices studentServices = new StudentServices();
+
+Student student = new Student
+{
+    Fullname = "Farhod Chimaev",
+    Age = 32,
+    Score =99
+};
+Console.WriteLine(studentServices.CreateStudent(student));
+Console.WriteLine(studentServices.GetStudents());
+Console.WriteLine(studentServices.GetStudentById(2));
+student.Id = 5;
+Console.WriteLine(studentServices.Update(student));
+Console.WriteLine(studentServices.Delete(1));
